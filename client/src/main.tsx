8b0db3332951535dab3d81774e7f3b96e45963a0
dave-8bit <chinwenmerid@gmail.com>
2026-06-12 02:07:50 +0100

    feat: add GameProvider to main app component with React StrictMode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
+import App from './App'
+import { GameProvider } from './context/GameContext'
+
+// Mount the app and provide game state via context.
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <GameProvider>
+      <App />
+    </GameProvider>
+  </React.StrictMode>,
 )
+
