df42b7d7b79912857691518621aaf8ba73dfaceb
dave-8bit <chinwenmerid@gmail.com>
2026-06-18 09:51:55 +0100

    feat: add eslint-disable comment for react-refresh in GameContext.tsx

--- client/src/context/GameContext.tsx
+++ client/src/context/GameContext.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useContext, useReducer } from 'react'
+
 import type { ReactNode } from 'react'
 
 import { gameReducer } from './gameReducer'
